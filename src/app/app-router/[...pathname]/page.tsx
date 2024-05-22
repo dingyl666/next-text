6e00b4ad6051af2a8fca230a071bef2e688b4601
dingyl666 <1811239843@qq.com>
2024-05-22 11:22:09 +0800

    获取数据

--- src/app/app-router/[...pathname]/page.tsx
+++ src/app/app-router/[...pathname]/page.tsx
@@ -4,6 +4,7 @@ export default async function (props: any) {
   return (
     <>
       动态路由:pages/post/[...id].js，那么它将匹配 /post/1，/post/1/2，/post/1/2/3 等等
+      与[[...pathname]]的唯一区别就是 后者包含/post路由 而 [...id]不包含 /post
     </>
   )
 }
