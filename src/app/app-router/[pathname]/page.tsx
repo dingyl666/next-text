b0ae5cc19e05ff8dcebaa4576a0468ba1757a173
dingyanlei <dingyanlei@guopaigaoke.com>
2025-06-20 14:58:28 +0800

    报错

--- src/app/app-router/[pathname]/page.tsx
+++ src/app/app-router/[pathname]/page.tsx
@@ -1,7 +1,9 @@
 
 
-export default async function (props:{params:{pathname:string}}) {
+export default async function (props:{params:Promise<{pathname:string}> }) {
+  const {pathname} = await props.params
   return <>
+  <p>pathname:{pathname}</p>
     动态路由:pages/post/[[...id]].js，那么它将匹配 /post，/post/1，/post/1/2，/post/1/2/3 等等
     <p>与[...id]的区别是 如果没有设置/post，他会捕获/post</p>
   </>
