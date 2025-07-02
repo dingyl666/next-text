a92ce77d7936b5b5ee183ec5eab8111f03ff03d4
dingyanlei <dingyanlei@guopaigaoke.com>
2025-07-02 14:59:10 +0800

    tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import Head from "next/head";
-
+import './globals.css'
 export default function Page() {
   return <>
     <Head>app router</Head>
+    <span className="text-red-500 ">hello world</span>
     <Link href={'/page-router'}><h1>pages router</h1></Link>
     <Link href={'/app-router'} scroll={false}><h1>app router</h1></Link>
   </>
